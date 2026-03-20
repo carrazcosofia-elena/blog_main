85d303c90b05029a86c584f3df970cc5984914bd
carrazcosofia-elena <sofiacarrazcop@gmail.com>
2026-03-20 15:55:35 -0700

    guardado admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,23 @@ from django.contrib import admin
 # Register your models here.
 from .models import Comment, Category, Post
 
+class CommentItemInline(admin.TabularIntline):
+    model = Commentraw_id_fields = ['post']
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'intro', 'body']
+    list_display = ('title', 'category', 'created_at', 'status')
+    list_filter = ['category', 'created_at', 'status']
+    inlines = [CommentItemInline]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+  search_fields = ['title']
+  list_display = ('title')
+
+class CommentAdmin(admin.ModelAdmin):
+   list_display = ('name', 'post', 'created_at')
+
 admin.site.register(Category)
 admin.site.register(Post)
 admin.site.register(Comment)
